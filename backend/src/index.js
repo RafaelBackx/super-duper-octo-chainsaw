6bacd3f57700df1192f22fbfe64d06da422b1b32
Rafael Backx <rafael@sentigrate.com>
2024-07-20 12:59:23 +0200

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use('/users', userRoutes);
 // Error handler
 app.use((error, req, res, next) => {
-    console.log(error.message);
+    console.log(error);
     return res.status(500).send(error.message)
 })
 
